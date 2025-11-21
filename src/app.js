0b598f1047e2267021772801e50a2bfe4563b9e7
Muhammad Naufal Yusuf <skynaufal738@gmail.com>
2025-11-21 23:32:59 +0700

    chore: set express to trust proxy (#24)

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(cors({
   origin: process.env.FRONTEND_URL,
